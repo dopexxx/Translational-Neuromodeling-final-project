78d27b622757fdd31f8d9637392d705f6ae69b2d
Moritz Gruber <mogruber@student.ethz.ch>
2018-05-19 19:54:43 +0200

    playtone for neutral stimulus

---
+++ playtone.m
@@ -0,0 +1,30 @@
+function [time,tone] = playtone(frequency,duration)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Generates and plays a sine wave stimulus of specified duration and 
+% frequency. The onset is windowed.
+% 
+% Moritz Gruber, May 2018
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Unpack inputs, set constants
+% -------------------------------------------------------------------------
+d = duration;
+f = frequency;
+sf = 44100;
+nsamples = sf*d;
+time = 0:1/sf:d;
+
+% Generate sine wave, and window
+% -------------------------------------------------------------------------
+
+tone = sin(2*pi*f*time);
+win_1 = hamming(nsamples)';
+win_2 = ones(1,nsamples);
+win = [win_1(1:nsamples/2) win_2(nsamples/2:end)];
+
+% Apply window, play sound
+% -------------------------------------------------------------------------
+
+tone = win.*tone;
+sound(tone,sf)
+end
